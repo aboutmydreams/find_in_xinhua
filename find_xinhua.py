f6283b3a0588a023a40dc28b7456761d7ccb5866
Sourcery AI <>
2022-05-30 17:57:16 +0000

    'Refactored by Sourcery'

--- find_xinhua.py
+++ find_xinhua.py
@@ -2,13 +2,12 @@ import requests,json
 
 
 def find_it(word,page_num):
-    url = "http://so.news.cn/getNews?keyword={}&curPage={}&sortField=0&searchFields=0&lang=cn".format(word,page_num)
+    url = f"http://so.news.cn/getNews?keyword={word}&curPage={page_num}&sortField=0&searchFields=0&lang=cn"
+
     res = requests.get(url).text
     data = json.loads(res)
     my_list = data["content"]["results"]
-    all_data = []
-    for one_data in my_list:
-        all_data.append(one_data["url"])
+    all_data = [one_data["url"] for one_data in my_list]
     print(all_data)
 
 
